6e1e42a82475acf8267e193e248dcc8b231c6057
JAstbury <jess.astbury@hotmail.com>
2016-09-06 10:19:42 +0100

    Initial Commit

---
+++ FizzBuzzSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FizzBuzzSwift
+//
+//  Created by Jess Astbury on 06/09/2016.
+//  Copyright © 2016 Jess Astbury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
